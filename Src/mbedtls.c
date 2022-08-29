6a06c85ee466b97191e6e8c37dba4c97c3b596e5
xiaofei <alibaba22001@163.com>
2022-08-29 13:50:17 +0800

    update

--- Src/mbedtls.c
+++ Src/mbedtls.c
@@ -8,13 +8,12 @@
   ******************************************************************************
    * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
   *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
   */
@@ -24,7 +23,7 @@
 #include "mbedtls.h"
 
 /* USER CODE BEGIN 0 */
-#include "net_sockets.h"
+
 /* USER CODE END 0 */
 
 /* USER CODE BEGIN 1 */
@@ -43,6 +42,7 @@ void MX_MBEDTLS_Init(void)
    /**
   */
   /* USER CODE BEGIN 3 */
+
   /* USER CODE END 3 */
 
 }
